e8d634ebc31cbc61b725cf689a9d26599969b77c
Nadeem Jamali <nadeem@nadeems-MacBook-Pro.local>
2018-06-07 20:02:30 +0500

    added all code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { YadagModule } from 'yadag';
+
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { DetailComponent } from './detail/detail.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    DetailComponent,
+    AddComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    YadagModule
   ],
   providers: [],
   bootstrap: [AppComponent]
